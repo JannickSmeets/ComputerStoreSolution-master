63e8a1eb590dd8940971bcbd2b02b4e3eea2de8d
JannickSmeets <jannick_smeets@hotmail.com>
2017-10-09 11:39:12 +0200

    Initial commit

---
+++ src/be/pxl/computerstore/hardware/WebCam.java
@@ -0,0 +1,9 @@
+package be.pxl.computerstore.hardware;
+
+public class WebCam extends Peripheral {
+
+	public WebCam(String vendor, String name, double price) {
+		super(vendor, name, price);
+	}
+
+}
